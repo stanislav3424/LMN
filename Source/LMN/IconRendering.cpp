42aa46b33e84b2e309a3804a70387a734c487846
Stanislav <dudoladov_stanislav@mail.ru>
2025-12-15 13:55:56 +0300

    11

---
+++ Source/LMN/IconRendering.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "IconRendering.h"
+
+// Sets default values
+AIconRendering::AIconRendering()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AIconRendering::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AIconRendering::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
