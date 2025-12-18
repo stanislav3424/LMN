6bcab928278cc607ca3a13956e9b838915d99fff
Stanislav <dudoladov_stanislav@mail.ru>
2025-12-18 16:38:27 +0300

    Add WidgetComponent support for all widgets inherited from UW_Base

---
+++ Source/LMN/ProgressBarWidgetComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WidgetComponentBase.h"
+#include "ProgressBarWidgetComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LMN_API UProgressBarWidgetComponent : public UWidgetComponentBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/LMN/WidgetComponentBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetComponent.h"
+#include "WidgetComponentBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LMN_API UWidgetComponentBase : public UWidgetComponent
+{
+	GENERATED_BODY()
+	
+};
