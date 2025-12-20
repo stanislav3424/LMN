d1ca8bd80f6acb122e964b665f30fd38b701f2dd
Stanislav <dudoladov_stanislav@mail.ru>
2025-12-20 13:15:45 +0300

    Add AI

---
+++ Source/LMN/AIControllerBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DetourCrowdAIController.h"
+#include "AIControllerBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LMN_API AAIControllerBase : public ADetourCrowdAIController
+{
+	GENERATED_BODY()
+	
+};
