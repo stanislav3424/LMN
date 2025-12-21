e715505686dbe292f2c947517db4d83834b68834
Stanislav <dudoladov_stanislav@mail.ru>
2025-12-21 19:18:32 +0300

    Add ABP

---
+++ Source/LMN/TTask_MoveType.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_MoveTo.h"
+#include "TTask_MoveType.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LMN_API UTTask_MoveType : public UBTTask_MoveTo
+{
+	GENERATED_BODY()
+	
+};
