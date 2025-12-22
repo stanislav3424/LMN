7df38a2f8a9b4163fb5b0bf28b8b4d1e178b3558
Stanislav <dudoladov_stanislav@mail.ru>
2025-12-22 16:22:40 +0300

    Add spawn helpers

---
+++ Source/LMN/InventoryLogic.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EquipmentLogic.h"
+#include "InventoryLogic.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LMN_API UInventoryLogic : public UEquipmentLogic
+{
+	GENERATED_BODY()
+	
+};
