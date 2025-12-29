56c0490dd94ba9e184fc724daf35e6891c655528
Stanislav <dudoladov_stanislav@mail.ru>
2025-12-29 12:21:03 +0300

    Fix run and add UW_Menu

---
+++ Source/LMN/UW_Menu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UW_Global.h"
+#include "UW_Menu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LMN_API UUW_Menu : public UUW_Global
+{
+	GENERATED_BODY()
+	
+};
