5bdc0649a55d2653a9b00212ab43fd1f97726afe
Stanislav <dudoladov_stanislav@mail.ru>
2026-01-03 14:08:50 +0300

    Add EQS and Task

---
+++ Source/LMN/BTTask_RunEQS_FindShelter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BTTask_RunEQS_FindShelter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LMN_API UBTTask_RunEQS_FindShelter : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+	
+};
