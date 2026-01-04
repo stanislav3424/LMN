cd734c6100e9194d6c36f15e9eb5cfebfddff5d6
Stanislav <dudoladov_stanislav@mail.ru>
2026-01-04 12:42:43 +0300

    Replacing logic with EQS_Test

---
+++ Source/LMN/EnvQueryTest_Shelter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnvironmentQuery/EnvQueryTest.h"
+#include "EnvQueryTest_Shelter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LMN_API UEnvQueryTest_Shelter : public UEnvQueryTest
+{
+	GENERATED_BODY()
+	
+};
