2a621ce614b2bab10d403207091876a05f9741f7
Stanislav <dudoladov_stanislav@mail.ru>
2026-01-09 11:59:59 +0300

    refactoring

---
+++ Source/LMN/EnvQueryTest_GetEnemies.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnvironmentQuery/EnvQueryTest.h"
+#include "EnvQueryTest_GetEnemies.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LMN_API UEnvQueryTest_GetEnemies : public UEnvQueryTest
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/LMN/GridLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "GridLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LMN_API UGridLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
