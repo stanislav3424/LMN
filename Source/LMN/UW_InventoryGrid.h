64beb6339d6b2cdbb65c5ca14931382cbaafe250
Stanislav <dudoladov_stanislav@mail.ru>
2026-01-23 17:22:27 +0300

    the logic of widget initialization has been changed

---
+++ Source/LMN/UW_InventoryGrid.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UW_Base.h"
+#include "UW_InventoryGrid.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LMN_API UUW_InventoryGrid : public UUW_Base
+{
+	GENERATED_BODY()
+	
+};
