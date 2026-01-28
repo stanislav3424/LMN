18fe0467959a2e8f3dbfb8770527d86ed70feb7e
Stanislav <dudoladov_stanislav@mail.ru>
2026-01-28 18:05:57 +0300

    Add item Drop

---
+++ Source/LMN/UW_ItemAutoSize.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UW_Item.h"
+#include "UW_ItemAutoSize.generated.h"
+
+UCLASS()
+class LMN_API UUW_ItemAutoSize : public UUW_Item
+{
+	GENERATED_BODY()
+	
+};
